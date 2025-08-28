74c4d11c9ca8d7bbe044b8a0452f0638933163d5
gushags <jeff@hagkull.com>
2025-08-28 15:02:12 -0700

    Chore: create file structure

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('node:path');
+const app = express();
+
+// Get application ready to use ejs templates
+// from 'views' folder
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
+const indexRouter = require('./routes/indexRouter');
+
+app.use('/', indexRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  // This is important!
+  // Without this, any startup errors will silently fail
+  // instead of giving you a helpful error message.
+  if (error) {
+    throw error;
+  }
+  console.log(`Mini Message Board app - listening on port ${PORT}!`);
+});
---
+++ routes/indexRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+const indexRouter = Router();
+
+indexRouter.get('/', (req, res) => {
+  res.render('index', {
+    copy: 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+  });
+});
+
+module.exports = indexRouter;
