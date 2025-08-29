b00881b74b52d60afa6cc5cc7c2d9e6024b7bf10
gushags <jeff@hagkull.com>
2025-08-28 20:24:20 -0700

    Feature: add form to submit new posts

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,10 +3,21 @@ const { Router } = require('express');
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 indexRouter.get('/', (req, res) => {
-  res.render('index', {
-    copy: 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
-  });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
 module.exports = indexRouter;
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -4,7 +4,10 @@ const { Router } = require('express');
 const newRouter = Router();
 
 newRouter.get('/', (req, res) => {
-  res.render('new', { newcopy: 'Testing this copy.' });
+  res.render('form');
+});
+newRouter.post('/', (req, res) => {
+  res.send('<p>Form needs to be submitted now</p>');
 });
 
 module.exports = newRouter;
