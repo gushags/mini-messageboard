9aaf108cc711ff84f38087af23981d83495a5758
gushags <jeff@hagkull.com>
2025-08-29 08:53:34 -0700

    Fix: display single message logic

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,16 +3,15 @@ const { Router } = require('express');
 const messages = require('../api/db');
 
 const indexRouter = Router();
-const { getMessageByDateAdded } = require('../controllers/indexController');
 
 indexRouter.get('/', (req, res) => {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
-indexRouter.get('/:messageName', (req, res) => {
-  const messageName = req.params.messageName; // Get the user's name from the URL parameter
-  const { message } = messages.find((message) => message.name === messageName);
-  res.render('message', { message: message }); // Render 'user.ejs' and pass the userName variable
+indexRouter.get('/:messageUser', (req, res) => {
+  const messageUser = req.params.messageUser; // Get the user's name from the URL parameter
+  const message = messages.find((message) => message.user === messageUser);
+  res.render('message', { message: message }); // Render 'message.ejs' and pass the message variable
 });
 
 module.exports = indexRouter;
